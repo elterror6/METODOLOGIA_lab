986c4bae418d3bd863a333940d470143e20d7ec1
elterror6 <danibasketball2000@gmail.com>
2025-03-29 16:21:04 +0100

    Add javadoc

--- CasosEstudio3_4/src/CasoEstudio1/Negocio/Coordinate.java
+++ CasosEstudio3_4/src/CasoEstudio1/Negocio/Coordinate.java
@@ -1,11 +1,33 @@
 package CasoEstudio1.Negocio;
 
+// TODO: Auto-generated Javadoc
+/**
+ * Esta clase representa una coordenada xy, pero guardando el anterior valor
+ * de cada coordenada. Esta clase esta creada exclusivamente para poder utilizarla
+ * en el caso de estudio 3 para poder localizar donde se va a insertar una baldosa.
+ * 
+ * @author Daniel Diaz Garcia
+ */
 public class Coordinate {
+	
+	/** La coordenada x. */
 	private int x;
+	
+	/** La coordenada y. */
 	private int y;
+	
+	/** La antigua coordenada x. */
 	private int oldX;
+	
+	/** La antigua coordenada y. */
 	private int oldY;
 	
+	/**
+	 * Instancia una nueva coordenada xy.
+	 *
+	 * @param x La coordenada x
+	 * @param y La coordenada y
+	 */
 	public Coordinate (int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -13,28 +35,60 @@ public class Coordinate {
 		this.oldY = this.y;
 	}
 
+	/**
+	 * Devuelve el actual de la coordenada x.
+	 *
+	 * @return El valor de la coordenada x.
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Introduce un nuevo valor para la coordenada x. El valor anterior
+	 * de la coordenada x pasa a ser la antigua  coordenada de x.
+	 *
+	 * @param x El nuevo valor de la coordenada x.
+	 */
 	public void setX(int x) {
 		this.oldX = this.x;
 		this.x = x;
 	}
 
+	/**
+	 * Devuelve el actual de la coordenada y.
+	 *
+	 * @return El valor de la coordenada y.
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Introduce un nuevo valor para la coordenada y. El valor anterior
+	 * de la coordenada x pasa a ser la antigua  coordenada de y.
+	 *
+	 * @param y El nuevo valor de la coordenada y
+	 */
 	public void setY(int y) {
 		this.oldY = this.y;
 		this.y = y;
 	}
 
+	/**
+	 * Devuelve el antiguo valor de la coordenada x.
+	 *
+	 * @return El antiguo valor de la coordenada x.
+	 */
 	public int getOldX() {
 		return oldX;
 	}
 
+	/**
+	 * GDevuelve el antiguo valor de la coordenada y.
+	 *
+	 * @return El antiguo valor de la coordenada y.
+	 */
 	public int getOldY() {
 		return oldY;
 	}
--- CasosEstudio3_4/src/CasoEstudio1/Negocio/GreedyAlgorithm.java
+++ CasosEstudio3_4/src/CasoEstudio1/Negocio/GreedyAlgorithm.java
@@ -16,17 +16,19 @@ public class GreedyAlgorithm {
 	 * @return the int[][]
 	 */
 	public int[][] tileFloor (Integer[] tiles, int n) {
-		int current = 0, roomSize=0, oldTileX, oldTileY;
+		int current = 0, roomSize = 0, oldTileY = 0;
 		int[][] solution = null;
 		Coordinate c = new Coordinate (0,0);
 		//TODO: Método para inicializar la solución.
 		Arrays.sort(tiles, Collections.reverseOrder());
 		
 		do {
-			if (tileFits(solution, tiles[current],c)){ 
+			if (tileFits(solution, tiles[current],c,oldTileY)){ 
 				//TODO: Método para reducir el roomSize
 				addTile (solution, c, tiles[current]);
 			} else {
+				oldTileY = c.getY();
+				c.setY(0);
 				++current;
 			}
 		}while(roomSize>0 || current < tiles.length);
@@ -45,22 +47,32 @@ public class GreedyAlgorithm {
 	 * @param c El objeto Coordinate que indica donde va a estar la baldosa.
 	 * @return true, si la baldosa de tamaño m cabe en el suelo.
 	 */
-	private boolean tileFits (int[][] a, int m, Coordinate c) {
+	private boolean tileFits (int[][] a, int m, Coordinate c, int oldTileY) {
 		boolean solution = true;
 		if (c.getX()+m > a[c.getY()].length && c.getY()+m > a.length) {
 			solution = false;
-		} else if (c.getX()+m < a[c.getY()].length) {
+		} else if (c.getX()+m <= a[c.getY()].length) {
 			if (c.getY() == 0) {
 				c.setY(m);
 			}
 			c.setX(c.getX()+m);
-		} else if (c.getY()+m < a.length) {
-			c.setX(0);
+		} else if (c.getY()+m <= a.length) {
+			if (c.getOldY() >= oldTileY) {
+				c.setX(0);
+			} else {
+				c.setX(c.getOldX());
+			}
 			c.setY(c.getY()+m);
 		}
 		return solution;
 	}
-	
+	/**
+	 * Este método se encarga de añadir la baldosa al suelo.
+	 * 
+	 * @param a El array que representa el suelo.
+	 * @param c El objeto coordenada que contiene donde se va a meter.
+	 * @param m El tamaño de la baldosa.
+	 */
 	private void addTile (int[][] a, Coordinate c, int m) {
 		for (int i = c.getOldY(); i <= c.getY(); i++) {
 			for (int j = c.getOldX(); j <= c.getX(); j++) {
