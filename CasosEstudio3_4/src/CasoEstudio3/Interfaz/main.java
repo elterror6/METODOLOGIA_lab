1e70c92c7066900a4b6afc2d2bf8d76bc2a831c4
OUSSAMA BOLBAROUD <oussama.bolbaroud@alu.uclm.es>
2025-04-10 17:28:13 +0200

    cambiar nombre de carpeta

---
+++ CasosEstudio3_4/src/CasoEstudio3/Interfaz/main.java
@@ -0,0 +1,45 @@
+package CasoEstudio3.Interfaz;
+
+import java.util.Scanner;
+
+import CasoEstudio3.Negocio.GreedyAlgorithm;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Solado Económico ===");
+
+        // Pedir tamaño del suelo (lado del cuadrado)
+        System.out.print("Introduce el tamaño del suelo (lado en metros): ");
+        int size = scanner.nextInt();
+        int [][] floor;
+
+        // Pedir número de tipos de baldosas
+        System.out.print("Introduce el número de tipos de baldosas: ");
+        int numTipos = scanner.nextInt();
+
+        // Pedir tamaño de cada baldosa
+        Integer [] tileSizes = new Integer [numTipos];
+        for (int i = 0; i < numTipos; i++) {
+            System.out.print("Introduce el tamaño de la baldosa #" + (i + 1) + " (en metros): ");
+            tileSizes[i] = scanner.nextInt();
+        }
+
+        // Mostrar los datos recogidos
+        System.out.println("\n--- Datos introducidos ---");
+        System.out.println("Tamaño del suelo: " + size + " x " + size + " metros");
+        
+        floor =  GreedyAlgorithm.tileFloor(tileSizes, size);
+        for(int i = 0; i<floor.length; i++) {
+        	for(int j = 0; j<floor[i].length;j++) {
+        		System.out.print(floor[i][j]+" ");
+        	}
+        	System.out.println();
+        }
+        // Aquí iría la llamada al algoritmo voraz 
+
+        scanner.close();
+    }
+}
--- CasosEstudio3_4/src/CasoEstudio3/Negocio/GreedyAlgorithm.java
+++ CasosEstudio3_4/src/CasoEstudio3/Negocio/GreedyAlgorithm.java
@@ -1,4 +1,4 @@
-package CasoEstudio1.Negocio;
+package CasoEstudio3.Negocio;
 
 import java.util.Arrays;
 import java.util.Collections;
